b3044ba33b0ea596fb879d0356b0f1cb6224600e
Liu Bing <liubinging@gmail.com>
2012-04-07 19:57:59 +0800

    New demands list changes.

--- CHSTRAS_Test/CHSTRAS_091_TestV091.Designer.cs
+++ CHSTRAS_Test/CHSTRAS_091_TestV091.Designer.cs
@@ -28,9 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.总界面 = new System.Windows.Forms.TabPage();
+            this.认识器物教学 = new System.Windows.Forms.TabPage();
+            this.认识器物游戏 = new System.Windows.Forms.TabPage();
+            this.tabPage桌面应用.SuspendLayout();
+            this.tabControlUI.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControlUI
+            // 
+            this.tabControlUI.Controls.Add(this.总界面);
+            this.tabControlUI.Controls.Add(this.认识器物教学);
+            this.tabControlUI.Controls.Add(this.认识器物游戏);
+            this.tabControlUI.Controls.SetChildIndex(this.认识器物游戏, 0);
+            this.tabControlUI.Controls.SetChildIndex(this.认识器物教学, 0);
+            this.tabControlUI.Controls.SetChildIndex(this.总界面, 0);
+            this.tabControlUI.Controls.SetChildIndex(this.tabPage桌面应用, 0);
+            // 
+            // 总界面
+            // 
+            this.总界面.Location = new System.Drawing.Point(4, 21);
+            this.总界面.Name = "总界面";
+            this.总界面.Padding = new System.Windows.Forms.Padding(3);
+            this.总界面.Size = new System.Drawing.Size(641, 68);
+            this.总界面.TabIndex = 1;
+            this.总界面.Text = "总界面";
+            this.总界面.UseVisualStyleBackColor = true;
+            // 
+            // 认识器物教学
+            // 
+            this.认识器物教学.Location = new System.Drawing.Point(4, 21);
+            this.认识器物教学.Name = "认识器物教学";
+            this.认识器物教学.Size = new System.Drawing.Size(641, 68);
+            this.认识器物教学.TabIndex = 2;
+            this.认识器物教学.Text = "认识器物教学";
+            this.认识器物教学.UseVisualStyleBackColor = true;
+            // 
+            // 认识器物游戏
+            // 
+            this.认识器物游戏.Location = new System.Drawing.Point(4, 21);
+            this.认识器物游戏.Name = "认识器物游戏";
+            this.认识器物游戏.Size = new System.Drawing.Size(641, 68);
+            this.认识器物游戏.TabIndex = 3;
+            this.认识器物游戏.Text = "认识器物游戏";
+            this.认识器物游戏.UseVisualStyleBackColor = true;
+            // 
+            // CHSTRAS_091_TestV091
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.ClientSize = new System.Drawing.Size(892, 566);
+            this.Name = "CHSTRAS_091_TestV091";
+            this.tabPage桌面应用.ResumeLayout(false);
+            this.tabControlUI.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabPage 总界面;
+        private System.Windows.Forms.TabPage 认识器物教学;
+        private System.Windows.Forms.TabPage 认识器物游戏;
     }
 }
