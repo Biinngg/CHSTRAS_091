2b0dd456bbdde3c41e4779f48373aea55648a63e
Liu Bing <liubinging@gmail.com>
2012-04-15 16:21:02 +0800

    Added the course structure.

--- CHSTRAS_UI/uiCHSTRAS_091_CourseShow.Designer.cs
+++ CHSTRAS_UI/uiCHSTRAS_091_CourseShow.Designer.cs
@@ -29,16 +29,17 @@
         private void InitializeComponent()
         {
             this.groupBox = new System.Windows.Forms.GroupBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.buttonBefore = new System.Windows.Forms.Button();
             this.buttonNext = new System.Windows.Forms.Button();
+            this.ucCourseManagerSimple1 = new SHF_UI.ucCourseManagerSimple();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHead)).BeginInit();
             this.groupBox.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label程序信息
@@ -62,50 +63,50 @@
             this.groupBox.Controls.Add(this.label1);
             this.groupBox.Controls.Add(this.pictureBox2);
             this.groupBox.Controls.Add(this.pictureBox1);
-            this.groupBox.Location = new System.Drawing.Point(38, 120);
+            this.groupBox.Location = new System.Drawing.Point(205, 120);
             this.groupBox.Name = "groupBox";
-            this.groupBox.Size = new System.Drawing.Size(576, 280);
+            this.groupBox.Size = new System.Drawing.Size(426, 280);
             this.groupBox.TabIndex = 148;
             this.groupBox.TabStop = false;
             this.groupBox.Text = "教学内容";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(48, 51);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 150);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(324, 51);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(200, 150);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(117, 235);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 12);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "图片1";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(403, 235);
+            this.label2.Location = new System.Drawing.Point(291, 235);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 12);
             this.label2.TabIndex = 3;
             this.label2.Text = "图片2";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "图片1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(212, 51);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 150);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 150);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // buttonBefore
             // 
-            this.buttonBefore.Location = new System.Drawing.Point(211, 435);
+            this.buttonBefore.Location = new System.Drawing.Point(261, 435);
             this.buttonBefore.Name = "buttonBefore";
             this.buttonBefore.Size = new System.Drawing.Size(75, 23);
             this.buttonBefore.TabIndex = 149;
@@ -114,23 +115,42 @@
             // 
             // buttonNext
             // 
-            this.buttonNext.Location = new System.Drawing.Point(362, 435);
+            this.buttonNext.Location = new System.Drawing.Point(476, 435);
             this.buttonNext.Name = "buttonNext";
             this.buttonNext.Size = new System.Drawing.Size(75, 23);
             this.buttonNext.TabIndex = 150;
             this.buttonNext.Text = "下一题";
             this.buttonNext.UseVisualStyleBackColor = true;
             // 
+            // ucCourseManagerSimple1
+            // 
+            this.ucCourseManagerSimple1.AcdemicPoint = 1;
+            this.ucCourseManagerSimple1.ChapterName = "";
+            this.ucCourseManagerSimple1.CourseName = "";
+            this.ucCourseManagerSimple1.FieldName = "";
+            this.ucCourseManagerSimple1.Location = new System.Drawing.Point(30, 92);
+            this.ucCourseManagerSimple1.Name = "ucCourseManagerSimple1";
+            this.ucCourseManagerSimple1.PageName = "";
+            this.ucCourseManagerSimple1.PointName = "";
+            this.ucCourseManagerSimple1.Size = new System.Drawing.Size(169, 421);
+            this.ucCourseManagerSimple1.StructureID = 0;
+            this.ucCourseManagerSimple1.TabIndex = 151;
+            this.ucCourseManagerSimple1.TerminalID = 0;
+            this.ucCourseManagerSimple1.TitleName = "";
+            this.ucCourseManagerSimple1.UnitName = "";
+            // 
             // uiCHSTRAS_091_CourseShow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.ClientSize = new System.Drawing.Size(792, 516);
             this.Controls.Add(this.groupBox);
-            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.ucCourseManagerSimple1);
             this.Controls.Add(this.buttonBefore);
+            this.Controls.Add(this.buttonNext);
             this.Name = "uiCHSTRAS_091_CourseShow";
-            this.Controls.SetChildIndex(this.buttonBefore, 0);
             this.Controls.SetChildIndex(this.buttonNext, 0);
+            this.Controls.SetChildIndex(this.buttonBefore, 0);
+            this.Controls.SetChildIndex(this.ucCourseManagerSimple1, 0);
             this.Controls.SetChildIndex(this.groupBox, 0);
             this.Controls.SetChildIndex(this.labelPageTitle, 0);
             this.Controls.SetChildIndex(this.label课程信息, 0);
@@ -144,8 +164,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHead)).EndInit();
             this.groupBox.ResumeLayout(false);
             this.groupBox.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,6 +180,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button buttonBefore;
         private System.Windows.Forms.Button buttonNext;
+        private SHF_UI.ucCourseManagerSimple ucCourseManagerSimple1;
 
     }
 }
--- CHSTRAS_UI/uiCHSTRAS_091_CourseShow.cs
+++ CHSTRAS_UI/uiCHSTRAS_091_CourseShow.cs
@@ -14,5 +14,20 @@ namespace CHSTRAS_091_UI
         {
             InitializeComponent();
         }
+
+        // 组合框选择事件
+        private void ucCourseManagerSimple1_comCourse_AfterSelect(object sender, EventArgs e)
+        {
+            label课程信息.Text = ucCourseManagerSimple1.FieldName + "|" + ucCourseManagerSimple1.CourseName + "|" + ucCourseManagerSimple1.UnitName;
+
+            label程序信息.Text = "《认识事物 CHSTRAS_091》V09-1|CHSTRAS_091_UI|uiCHSTRAS_091_CourseEdit";
+            if (ucCourseManagerSimple1.TerminalID > 0) labelPageTitle.Text = this.myPages.GetOne(ucCourseManagerSimple1.TerminalID).PageTitle;
+            else labelPageTitle.Text = this.ucCourseManagerSimple1.FieldName;
+        }
+
+        // TreeView控件选择事件
+        private void ucCourseManagerSimple1_trvCourse_AfterSelect(object sender, EventArgs e)
+        {
+        }
     }
 }
