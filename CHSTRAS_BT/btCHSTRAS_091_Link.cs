717bab82253b1ed0c2ee03d20f49d5377ac0878e
Liu Bing <liubinging@gmail.com>
2012-05-25 23:21:15 +0800

    Added but not used.

---
+++ CHSTRAS_BT/btCHSTRAS_091_Link.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SHF_BT;
+
+namespace CHSTRAS_091_BT
+{
+    class btCHSTRAS_091_Link : btCHSTRAS_091_Teach
+    {
+        public btCHSTRAS_091_Link(btSHFUserLogin shfUserLogin, btSHFUnitPractice shfUnitPratice)
+            : base(shfUserLogin, shfUnitPratice)
+        {
+
+        }
+    }
+}
