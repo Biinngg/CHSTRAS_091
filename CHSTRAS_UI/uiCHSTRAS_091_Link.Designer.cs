a58af941695f445a5f43917659a5532b136c6ca0
Liu Bing <liubinging@gmail.com>
2012-05-25 23:30:21 +0800

    Changed the Link ui to fit the Base class and optimized the interface.

--- CHSTRAS_UI/uiCHSTRAS_091_Link.Designer.cs
+++ CHSTRAS_UI/uiCHSTRAS_091_Link.Designer.cs
@@ -37,8 +37,7 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.buttonNext = new System.Windows.Forms.Button();
-            this.buttonBefore = new System.Windows.Forms.Button();
+            this.groupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHead)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
@@ -67,43 +66,54 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label4.Location = new System.Drawing.Point(405, 228);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.Padding = new System.Windows.Forms.Padding(5);
+            this.label4.Size = new System.Drawing.Size(51, 22);
             this.label4.TabIndex = 7;
             this.label4.Text = "label4";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label3.Location = new System.Drawing.Point(318, 228);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.Padding = new System.Windows.Forms.Padding(5);
+            this.label3.Size = new System.Drawing.Size(51, 22);
             this.label3.TabIndex = 6;
             this.label3.Text = "label3";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label2.Location = new System.Drawing.Point(233, 228);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.Padding = new System.Windows.Forms.Padding(5);
+            this.label2.Size = new System.Drawing.Size(51, 22);
             this.label2.TabIndex = 5;
             this.label2.Text = "label2";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label1.Location = new System.Drawing.Point(150, 228);
+            this.label1.Margin = new System.Windows.Forms.Padding(5);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.Padding = new System.Windows.Forms.Padding(5);
+            this.label1.Size = new System.Drawing.Size(51, 22);
             this.label1.TabIndex = 4;
             this.label1.Text = "label1";
             // 
             // pictureBox4
             // 
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox4.Location = new System.Drawing.Point(452, 42);
             this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Padding = new System.Windows.Forms.Padding(3);
             this.pictureBox4.Size = new System.Drawing.Size(140, 105);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 3;
@@ -111,8 +121,10 @@
             // 
             // pictureBox3
             // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox3.Location = new System.Drawing.Point(306, 42);
             this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Padding = new System.Windows.Forms.Padding(3);
             this.pictureBox3.Size = new System.Drawing.Size(140, 105);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 2;
@@ -120,8 +132,11 @@
             // 
             // pictureBox2
             // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.InitialImage = null;
             this.pictureBox2.Location = new System.Drawing.Point(152, 42);
             this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Padding = new System.Windows.Forms.Padding(3);
             this.pictureBox2.Size = new System.Drawing.Size(148, 105);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
@@ -129,40 +144,25 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.InitialImage = null;
             this.pictureBox1.Location = new System.Drawing.Point(6, 42);
             this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(3);
             this.pictureBox1.Size = new System.Drawing.Size(140, 105);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // buttonNext
-            // 
-            this.buttonNext.Location = new System.Drawing.Point(350, 435);
-            this.buttonNext.Name = "buttonNext";
-            this.buttonNext.Size = new System.Drawing.Size(75, 23);
-            this.buttonNext.TabIndex = 152;
-            this.buttonNext.Text = "下一题";
-            this.buttonNext.UseVisualStyleBackColor = true;
-            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
-            // 
-            // buttonBefore
-            // 
-            this.buttonBefore.Location = new System.Drawing.Point(229, 435);
-            this.buttonBefore.Name = "buttonBefore";
-            this.buttonBefore.Size = new System.Drawing.Size(75, 23);
-            this.buttonBefore.TabIndex = 151;
-            this.buttonBefore.Text = "上一题";
-            this.buttonBefore.UseVisualStyleBackColor = true;
-            // 
             // uiCHSTRAS_091_Link
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.ClientSize = new System.Drawing.Size(792, 516);
-            this.Controls.Add(this.buttonNext);
-            this.Controls.Add(this.buttonBefore);
             this.Controls.Add(this.groupBox1);
             this.Name = "uiCHSTRAS_091_Link";
+            this.Controls.SetChildIndex(this.buttonExit, 0);
+            this.Controls.SetChildIndex(this.groupBox, 0);
             this.Controls.SetChildIndex(this.groupBox1, 0);
             this.Controls.SetChildIndex(this.labelPageTitle, 0);
             this.Controls.SetChildIndex(this.label课程信息, 0);
@@ -173,8 +173,7 @@
             this.Controls.SetChildIndex(this.button退出, 0);
             this.Controls.SetChildIndex(this.button返回, 0);
             this.Controls.SetChildIndex(this.button确定, 0);
-            this.Controls.SetChildIndex(this.buttonBefore, 0);
-            this.Controls.SetChildIndex(this.buttonNext, 0);
+            this.groupBox.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHead)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -189,8 +188,6 @@
 
         #endregion
 
-        protected System.Windows.Forms.Button buttonNext;
-        protected System.Windows.Forms.Button buttonBefore;
         protected System.Windows.Forms.GroupBox groupBox1;
         protected System.Windows.Forms.PictureBox pictureBox4;
         protected System.Windows.Forms.PictureBox pictureBox3;
