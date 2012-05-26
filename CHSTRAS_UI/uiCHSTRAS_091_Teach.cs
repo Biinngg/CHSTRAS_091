0bd922b7cdf3bc150e783de6fb51274cb1bb8c79
Liu Bing <liubinging@gmail.com>
2012-05-26 21:41:09 +0800

    Update the statistics info.

--- CHSTRAS_UI/uiCHSTRAS_091_Teach.cs
+++ CHSTRAS_UI/uiCHSTRAS_091_Teach.cs
@@ -12,6 +12,7 @@ namespace CHSTRAS_091_UI
     public partial class uiCHSTRAS_091_Teach : uiCHSTRAS_091_Base
     {
         private btCHSTRAS_091_Teach bt;
+        private int totalNum;
 
         public uiCHSTRAS_091_Teach()
         {
@@ -28,13 +29,35 @@ namespace CHSTRAS_091_UI
             number = label.Length;
             bt = new btCHSTRAS_091_Teach(shfUserLogin, shfUnitPratice);
             buttonBackward.Enabled = false;
+            totalNum = bt.getTotal();
+            this.labelTotal.Text += totalNum;
             next();
         }
 
-        private void buttonForward_Click(object sender, EventArgs e)
+        protected void buttonForward_Click(object sender, EventArgs e)
         {
             buttonBackward.Enabled = true;
             next();
+            int progress = answeredNum * 100 / totalNum;
+            this.progressBar1.Value = progress;
+            Console.Write("value:" + progress + "\n answeredNum=" + answeredNum + " totalNum="+ totalNum);
+        }
+
+        protected void buttonBackward_Click(object sender, EventArgs e)
+        {
+            buttonForward.Enabled = true;
+            for (int i = number - 1; i >= 0; i--)
+            {
+                Boolean result = bt.moveToLast();
+                pictureBox[i].Image = bt.getImage();
+                label[i].Text = bt.getText();
+                if (!result)
+                {
+                    buttonBackward.Enabled = false;
+                }
+            }
+            int progress = answeredNum * 100 / totalNum;
+            this.progressBar1.Value = progress;
         }
 
         private void next()
@@ -47,21 +70,6 @@ namespace CHSTRAS_091_UI
                 if (!result)
                 {
                     buttonForward.Enabled = false;
-                }
-            }
-        }
-
-        private void buttonBackward_Click(object sender, EventArgs e)
-        {
-            buttonForward.Enabled = true;
-            for (int i = number - 1; i >= 0; i--)
-            {
-                Boolean result = bt.moveToLast();
-                pictureBox[i].Image = bt.getImage();
-                label[i].Text = bt.getText();
-                if (!result)
-                {
-                    buttonBackward.Enabled = false;
                 }
             }
         }
