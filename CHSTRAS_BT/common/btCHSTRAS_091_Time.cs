d81485e8a2d20a54efa537358ed8771803b08289
Liu Bing <liubinging@gmail.com>
2012-05-26 21:12:03 +0800

    Added the time format function and time passed.

---
+++ CHSTRAS_BT/common/btCHSTRAS_091_Time.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CHSTRAS_091_BT.Common
+{
+    public class btCHSTRAS_091_Time
+    {
+        public String getTime(DateTime dateTime)
+        {
+            String time = "";
+            if (dateTime.Hour < 10)
+                time += "0";
+            time += dateTime.Hour + ":";
+            if (dateTime.Minute < 10)
+                time += "0";
+            time += dateTime.Minute + ":";
+            if (dateTime.Second < 10)
+                time += "0";
+            time += dateTime.Second;
+            return time;
+        }
+        public String getTime(TimeSpan dateTime)
+        {
+            String time = "";
+            if (dateTime.Hours < 10)
+                time += "0";
+            time += dateTime.Hours + ":";
+            if (dateTime.Minutes < 10)
+                time += "0";
+            time += dateTime.Minutes + ":";
+            if (dateTime.Seconds < 10)
+                time += "0";
+            time += dateTime.Seconds;
+            return time;
+        }
+
+        public int getAnswerTime(DateTime dateTime)
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan diff = now - dateTime;
+            return (int)diff.TotalSeconds;
+        }
+    }
+}
