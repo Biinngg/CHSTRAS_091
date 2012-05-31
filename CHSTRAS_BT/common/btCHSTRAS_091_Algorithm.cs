a80ebfaaaace15837bdd090dd5cc8de28d3671da
Liu Bing <liubinging@gmail.com>
2012-06-01 00:55:51 +0800

    Might be used in puzzle game.

---
+++ CHSTRAS_BT/common/btCHSTRAS_091_Algorithm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CHSTRAS_091_BT.Common
+{
+    class btCHSTRAS_091_Algorithm
+    {
+        public String[] random(String[] array)
+        {
+            int size = array.Length;
+            String[] result = new String[size];
+            Random rd = new Random();
+            for(int i=0;i<size;i++)
+            {
+                result[i] = array[rd.Next(size)];
+            }
+            return result;
+        }
+    }
+}
