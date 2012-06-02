750a127390ea8b40794ab718b40ae6628c5790cb
Liu Bing <liubinging@gmail.com>
2012-06-02 23:33:36 +0800

    Added the Puzzle class.

---
+++ CHSTRAS_091_UI/uiCHSTRAS_091_Puzzle.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace CHSTRAS_091_UI
+{
+    partial class uiCHSTRAS_091_Puzzle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBoxR9 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxR8 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxR7 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxR6 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxR5 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxR4 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxR3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxR2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxR1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxL9 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxL8 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxL7 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxL6 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxL5 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxL4 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxL3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxL2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxL1 = new System.Windows.Forms.PictureBox();
+            this.groupBox.SuspendLayout();
+            this.groupBoxInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHead)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.groupBox1);
+            this.groupBox.Controls.SetChildIndex(this.buttonSubmit, 0);
+            this.groupBox.Controls.SetChildIndex(this.buttonForward, 0);
+            this.groupBox.Controls.SetChildIndex(this.buttonBackward, 0);
+            this.groupBox.Controls.SetChildIndex(this.labelStatistics, 0);
+            this.groupBox.Controls.SetChildIndex(this.groupBox1, 0);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBoxR9);
+            this.groupBox1.Controls.Add(this.pictureBoxR8);
+            this.groupBox1.Controls.Add(this.pictureBoxR7);
+            this.groupBox1.Controls.Add(this.pictureBoxR6);
+            this.groupBox1.Controls.Add(this.pictureBoxR5);
+            this.groupBox1.Controls.Add(this.pictureBoxR4);
+            this.groupBox1.Controls.Add(this.pictureBoxR3);
+            this.groupBox1.Controls.Add(this.pictureBoxR2);
+            this.groupBox1.Controls.Add(this.pictureBoxR1);
+            this.groupBox1.Controls.Add(this.pictureBoxL9);
+            this.groupBox1.Controls.Add(this.pictureBoxL8);
+            this.groupBox1.Controls.Add(this.pictureBoxL7);
+            this.groupBox1.Controls.Add(this.pictureBoxL6);
+            this.groupBox1.Controls.Add(this.pictureBoxL5);
+            this.groupBox1.Controls.Add(this.pictureBoxL4);
+            this.groupBox1.Controls.Add(this.pictureBoxL3);
+            this.groupBox1.Controls.Add(this.pictureBoxL2);
+            this.groupBox1.Controls.Add(this.pictureBoxL1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(605, 302);
+            this.groupBox1.TabIndex = 149;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "组合拼图答题";
+            // 
+            // pictureBoxR9
+            // 
+            this.pictureBoxR9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxR9.Location = new System.Drawing.Point(492, 165);
+            this.pictureBoxR9.Name = "pictureBoxR9";
+            this.pictureBoxR9.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxR9.TabIndex = 168;
+            this.pictureBoxR9.TabStop = false;
+            // 
+            // pictureBoxR8
+            // 
+            this.pictureBoxR8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxR8.Location = new System.Drawing.Point(412, 165);
+            this.pictureBoxR8.Name = "pictureBoxR8";
+            this.pictureBoxR8.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxR8.TabIndex = 167;
+            this.pictureBoxR8.TabStop = false;
+            // 
+            // pictureBoxR7
+            // 
+            this.pictureBoxR7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxR7.Location = new System.Drawing.Point(332, 165);
+            this.pictureBoxR7.Name = "pictureBoxR7";
+            this.pictureBoxR7.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxR7.TabIndex = 166;
+            this.pictureBoxR7.TabStop = false;
+            // 
+            // pictureBoxR6
+            // 
+            this.pictureBoxR6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxR6.Location = new System.Drawing.Point(492, 105);
+            this.pictureBoxR6.Name = "pictureBoxR6";
+            this.pictureBoxR6.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxR6.TabIndex = 165;
+            this.pictureBoxR6.TabStop = false;
+            // 
+            // pictureBoxR5
+            // 
+            this.pictureBoxR5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxR5.Location = new System.Drawing.Point(412, 105);
+            this.pictureBoxR5.Name = "pictureBoxR5";
+            this.pictureBoxR5.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxR5.TabIndex = 164;
+            this.pictureBoxR5.TabStop = false;
+            // 
+            // pictureBoxR4
+            // 
+            this.pictureBoxR4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxR4.Location = new System.Drawing.Point(332, 105);
+            this.pictureBoxR4.Name = "pictureBoxR4";
+            this.pictureBoxR4.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxR4.TabIndex = 163;
+            this.pictureBoxR4.TabStop = false;
+            // 
+            // pictureBoxR3
+            // 
+            this.pictureBoxR3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxR3.Location = new System.Drawing.Point(492, 45);
+            this.pictureBoxR3.Name = "pictureBoxR3";
+            this.pictureBoxR3.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxR3.TabIndex = 162;
+            this.pictureBoxR3.TabStop = false;
+            // 
+            // pictureBoxR2
+            // 
+            this.pictureBoxR2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxR2.Location = new System.Drawing.Point(412, 45);
+            this.pictureBoxR2.Name = "pictureBoxR2";
+            this.pictureBoxR2.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxR2.TabIndex = 161;
+            this.pictureBoxR2.TabStop = false;
+            // 
+            // pictureBoxR1
+            // 
+            this.pictureBoxR1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxR1.Location = new System.Drawing.Point(332, 45);
+            this.pictureBoxR1.Name = "pictureBoxR1";
+            this.pictureBoxR1.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxR1.TabIndex = 160;
+            this.pictureBoxR1.TabStop = false;
+            // 
+            // pictureBoxL9
+            // 
+            this.pictureBoxL9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxL9.Location = new System.Drawing.Point(195, 165);
+            this.pictureBoxL9.Name = "pictureBoxL9";
+            this.pictureBoxL9.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxL9.TabIndex = 159;
+            this.pictureBoxL9.TabStop = false;
+            // 
+            // pictureBoxL8
+            // 
+            this.pictureBoxL8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxL8.Location = new System.Drawing.Point(115, 165);
+            this.pictureBoxL8.Name = "pictureBoxL8";
+            this.pictureBoxL8.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxL8.TabIndex = 158;
+            this.pictureBoxL8.TabStop = false;
+            // 
+            // pictureBoxL7
+            // 
+            this.pictureBoxL7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxL7.Location = new System.Drawing.Point(35, 165);
+            this.pictureBoxL7.Name = "pictureBoxL7";
+            this.pictureBoxL7.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxL7.TabIndex = 157;
+            this.pictureBoxL7.TabStop = false;
+            // 
+            // pictureBoxL6
+            // 
+            this.pictureBoxL6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxL6.Location = new System.Drawing.Point(195, 105);
+            this.pictureBoxL6.Name = "pictureBoxL6";
+            this.pictureBoxL6.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxL6.TabIndex = 156;
+            this.pictureBoxL6.TabStop = false;
+            // 
+            // pictureBoxL5
+            // 
+            this.pictureBoxL5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxL5.Location = new System.Drawing.Point(115, 105);
+            this.pictureBoxL5.Name = "pictureBoxL5";
+            this.pictureBoxL5.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxL5.TabIndex = 155;
+            this.pictureBoxL5.TabStop = false;
+            // 
+            // pictureBoxL4
+            // 
+            this.pictureBoxL4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxL4.Location = new System.Drawing.Point(35, 105);
+            this.pictureBoxL4.Name = "pictureBoxL4";
+            this.pictureBoxL4.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxL4.TabIndex = 154;
+            this.pictureBoxL4.TabStop = false;
+            // 
+            // pictureBoxL3
+            // 
+            this.pictureBoxL3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxL3.Location = new System.Drawing.Point(195, 45);
+            this.pictureBoxL3.Name = "pictureBoxL3";
+            this.pictureBoxL3.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxL3.TabIndex = 153;
+            this.pictureBoxL3.TabStop = false;
+            // 
+            // pictureBoxL2
+            // 
+            this.pictureBoxL2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxL2.Location = new System.Drawing.Point(115, 45);
+            this.pictureBoxL2.Name = "pictureBoxL2";
+            this.pictureBoxL2.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxL2.TabIndex = 152;
+            this.pictureBoxL2.TabStop = false;
+            // 
+            // pictureBoxL1
+            // 
+            this.pictureBoxL1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxL1.Location = new System.Drawing.Point(35, 45);
+            this.pictureBoxL1.Name = "pictureBoxL1";
+            this.pictureBoxL1.Size = new System.Drawing.Size(80, 60);
+            this.pictureBoxL1.TabIndex = 151;
+            this.pictureBoxL1.TabStop = false;
+            // 
+            // uiCHSTRAS_091_Puzzle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 516);
+            this.Name = "uiCHSTRAS_091_Puzzle";
+            this.Text = "uiCHSTRAS_091_Puzzle";
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.groupBoxInfo.ResumeLayout(false);
+            this.groupBoxInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHead)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBoxR9;
+        private System.Windows.Forms.PictureBox pictureBoxR8;
+        private System.Windows.Forms.PictureBox pictureBoxR7;
+        private System.Windows.Forms.PictureBox pictureBoxR6;
+        private System.Windows.Forms.PictureBox pictureBoxR5;
+        private System.Windows.Forms.PictureBox pictureBoxR4;
+        private System.Windows.Forms.PictureBox pictureBoxR3;
+        private System.Windows.Forms.PictureBox pictureBoxR2;
+        private System.Windows.Forms.PictureBox pictureBoxR1;
+        private System.Windows.Forms.PictureBox pictureBoxL9;
+        private System.Windows.Forms.PictureBox pictureBoxL8;
+        private System.Windows.Forms.PictureBox pictureBoxL7;
+        private System.Windows.Forms.PictureBox pictureBoxL6;
+        private System.Windows.Forms.PictureBox pictureBoxL5;
+        private System.Windows.Forms.PictureBox pictureBoxL4;
+        private System.Windows.Forms.PictureBox pictureBoxL3;
+        private System.Windows.Forms.PictureBox pictureBoxL2;
+        private System.Windows.Forms.PictureBox pictureBoxL1;
+    }
+}
---
+++ CHSTRAS_091_UI/uiCHSTRAS_091_Puzzle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CHSTRAS_091_UI
+{
+    public partial class uiCHSTRAS_091_Puzzle : uiCHSTRAS_091_Base
+    {
+        private PictureBox[] pictureBoxL, pictureBoxR;
+
+        public uiCHSTRAS_091_Puzzle(Form form, SHF_BT.btSHFUserLogin shfUserLogin,
+            SHF_BT.btSHFUnitPractice shfUnitPratice, int courseType)
+            : base(form, shfUserLogin, shfUnitPratice, courseType)
+        {
+            InitializeComponent();
+        }
+    }
+}
